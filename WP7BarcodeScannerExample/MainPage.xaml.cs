c024e7aff461ee73ee389f55aeb85782d2c33f79
gzb1985 <gzb1985@gmail.com>
2012-06-17 14:32:26 +0800

    commit

---
+++ WP7BarcodeScannerExample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace WP7BarcodeScannerExample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void oneDBarcodeScan_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/BarCode.xaml", UriKind.Relative));
+        }
+
+        private void twoDBarcodeScan_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/BarCode.xaml?type=qrcode", UriKind.Relative));
+        }
+    }
+}
---
+++ WP7BarcodeScannerExample/ScanResult.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
+
+namespace WP7BarcodeScannerExample
+{
+    public partial class ScanResult : PhoneApplicationPage
+    {
+        public ScanResult()
+        {
+            InitializeComponent();
+            scanResult.Text = "";
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string result = "";
+            if (NavigationContext.QueryString.TryGetValue("result", out result))
+            {
+                scanResult.Text += result + "\r\n";
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            var lastPage = NavigationService.BackStack.FirstOrDefault();
+            if (lastPage != null && lastPage.Source.ToString().Contains("/BarCode.xaml"))
+            {
+                NavigationService.RemoveBackEntry();
+            }
+
+            base.OnNavigatingFrom(e);
+        }
+    }
+}
